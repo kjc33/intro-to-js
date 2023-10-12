21ac8c441acd29d03f5094e1050fe48224bb7969
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-10-11 21:05:34 -0700

    Added JS Examples

---
+++ scripts.js
@@ -0,0 +1,34 @@
+console.log("Hi from script.js");
+
+let legalName = "John Doe";
+console.log(legalName);
+
+let age;
+console.log("age before assignment", age)
+age = 38;
+console.log("age before assignment", age);
+
+
+
+let firstLetterOfTheAlphabet = "a1";
+
+let firstletterofthealphabet = "a2";
+
+console.log("camel case", firstLetterOfTheAlphabet);
+
+console.log("lower case", firstletterofthealphabet)
+
+const num = 1;
+console.log(num, typeof num);
+
+const string = "1";
+console.log(string, typeof string);
+
+let firstNum = 17;
+let secondNum = 4;
+
+console.log("Addition", firstNum + secondNum);
+console.log("Subtraction", firstNum - secondNum);
+console.log("Multiplication", firstNum * secondNum);
+console.log("Division", firstNum / secondNum);
+console.log("Modulus", firstNum % secondNum);
